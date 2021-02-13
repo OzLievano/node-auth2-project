3f068ed6ac518e11674637d3521e13791275bc59
Osvaldo Lievano <Osvaldoalievano@gmail.com>
2021-02-13 18:52:47 -0500

    login end point working successfully

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,10 +1,10 @@
 const bcryptjs = require('bcryptjs');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
 const router =require('express').Router();
 const Users = require('../../api/users/users-model');
 const { isValid } = require('../users/users-service');
-
+const secrets = require('../config/secrets')
 router.post('/register', function registerUser(req,res){
     const credentials = req.body;
 
@@ -28,6 +28,48 @@ router.post('/register', function registerUser(req,res){
         })
     }
 })
+
+router.post("/login", (req, res) => {
+    const { username, password } = req.body;
+  
+    if (isValid(req.body)) {
+      Users.findBy({ username: username })
+        .then(([user]) => {
+          // compare the password the hash stored in the database
+          if (user && bcryptjs.compareSync(password, user.password)) {
+  
+            const token = generateToken(user);
+  
+            res.status(200).json({ message: "Welcome to our API" , token});
+          } else {
+            res.status(401).json({ message: "You shall not PASS!" });
+          }
+        })
+        .catch(error => {
+          res.status(500).json({ message: error.message });
+        });
+    } else {
+      res.status(400).json({
+        message: "please provide username and password and the password shoud be alphanumeric",
+      });
+    }
+  });
+  
+  function generateToken(user){
+    const payload={
+      subject:user.id,
+      username:user.username,
+      role:user.role
+    }
+  
+    const options = {
+      expiresIn : '10h'
+    };
+  
+    const secret = secrets.jwtSecret
+  
+    return jwt.sign(payload,secret,options)
+  }
 
 
 module.exports = router;
